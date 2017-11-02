a7810b9dfeaa825f57d7b1b8e28b4235c6127324
Jonathan Wilberforce <jonforce52@hotmail.com>
2017-11-02 04:02:29 +0000

    excercise 4 incomplete

--- request.php
+++ request.php
@@ -14,5 +14,38 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name = "definition" author =jack>
+    A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition>
+       <definition name = "javascript" author =jill>
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition>
+        <definition name = "php" author =tim>
+         A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+    <definition>
+        <definition name = "ajax" author =bill>
+         A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+    <definition>
+       <definition name = "css" author =tom>
+         A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition>
+       <definition name = "html" author =jeff>
+       The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition>
+       <definition name = "bar" author =joe>
+       A place that sells alcholic beverages.
+    </definition>
+</entries>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
